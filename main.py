d228dbca47f8cc85d581316f24dec4c97d01c544
Camelot Yepez <113930338+cyepez2@users.noreply.github.com>
2022-11-09 18:26:06 +0000

    added stuff

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
-from dynamic_functions import check_3digits, all_positives, sum_less, count_even
-coffee_prices =[('cappucino', 1.5,),
-               ('expresso',1.2),
-               ('mocha',1.9)]
+# from dynamic_functions import check_3digits, all_positives, sum_less, count_even
+from function_interactions import mix_sticks, try_your_luck
+
+sticks = ["-", "--", "---", "----", "-----"]
+my_mix = mix_sticks(sticks)
+print(my_mix)
+
+number_guessed = try_your_luck()
+print(number_guessed)
+
+print(verify_my_try(my_mix, try1))
 
 
-for coffee, price in coffee_prices:
- print(f" the cost of one {coffee} is {price}")
-#create a function to get the highest priced coffee and its name
-def highroller(coffee_prices):
-  highest_price = 0
-  pricest_coffee = ''
-  for coffee, price in coffee_prices:
-    if price > highest_price:
-      highest_price = price 
-      most_expensive = coffee
-    else:
-      pass
-  return(most_expensive,highest_price)    
-print(highroller(coffee_prices))
+
+
+
+
+
+
+# coffee_prices =[('cappucino', 1.5,),
+#                ('expresso',1.2),
+#                ('mocha',1.9)]
+
+
+# for coffee, price in coffee_prices:
+#  print(f" the cost of one {coffee} is {price}")
+# #create a function to get the highest priced coffee and its name
+# def highroller(coffee_prices):
+#   highest_price = 0
+#   pricest_coffee = ''
+#   for coffee, price in coffee_prices:
+#     if price > highest_price:
+#       highest_price = price 
+#       most_expensive = coffee
+#     else:
+#       pass
+#   return(most_expensive,highest_price)    
+# print(highroller(coffee_prices))
  
 
-highroller(coffee_prices)
+# highroller(coffee_prices)
 
-data = check_3digits([191,999,610,191,11,531,511,566,911,711,696,420,117,118])
-print(data)
-data2 = all_positives([1,33,-1,55])
-print(data2)
-test = sum_less([1,10,25,50,999])
-print(test)
-test4 = count_even([55, 99, 600, 78, 120, 500, 20, 78, 333])
-print(test4)
+# data = check_3digits([191,999,610,191,11,531,511,566,911,711,696,420,117,118])
+# print(data)
+# data2 = all_positives([1,33,-1,55])
+# print(data2)
+# test = sum_less([1,10,25,50,999])
+# print(test)
+# test4 = count_even([55, 99, 600, 78, 120, 500, 20, 78, 333])
+# print(test4)
